279988ce3754e4cd7c254db7018b4cae5d4731d7
Soyeb Ali Faruque <soyebalifaruque@gmail.com>
2025-08-31 23:15:03 +0530

    update

---
+++ src/main/java/com/s5/modulink/ModulinkApplication.java
@@ -0,0 +1,13 @@
+package com.s5.modulink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModulinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/s5/modulink/ModulinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s5.modulink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
